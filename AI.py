0e2465b0d29eb8c0e681b4bc8d76638607ce6691
Jose-Colin <61718204+Jose-Colin@users.noreply.github.com>
2020-03-03 21:59:27 -0600

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,15 @@
+line='*******************'
+a=input(print('Name '))
+print ('Hola! {}'.format(a))
+print(line)
+e=input(print('¿Que edad tienes?'))
+if e>'18':
+	print('Genial ya eres legal')
+else:
+	print('Aun te falta para ser legal')
+print(line)
+b=input(print('Donde estudias?'))
+print('Nuy bien!'.format(b))
+print(line)
+c=input(print('Estudias licenciatura o ingenieria?'))
+print('Eso es todo!')
---
+++ profile.py
@@ -0,0 +1,16 @@
+LINE='***************'
+ID='193110698'
+NAME='Jose Roberto'
+LAST='Colin Avila'
+AGE= 18
+SING=False
+SPORTS=True
+
+print(LINE)
+print('{:10}{} {}'.format('NAME: ',NAME, LAST))
+print('{:10}{}'.format('AGE:',AGE))
+print(LINE)
+print("PRACTICE")
+print('{:10}{}'.format('ANY SPORT:', SPORTS))
+print('{:10}{}'.format('SING',SING))
+print(LINE)
